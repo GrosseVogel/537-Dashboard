a75ccb34046a860b4973098a0cbe2f7e6d96d641
Matt Kline <slavik262@gmail.com>
2011-07-30 10:11:08 -0500

    Added start of Dashboard Controls.
    Signed-off-by: Matt Kline <slavik262@gmail.com>

---
+++ Dashboard/DashboardControls/StatusLight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DashboardControls
+{
+    public partial class StatusLight : UserControl
+    {
+        private Pen forePen;
+
+        public StatusLight()
+        {
+            InitializeComponent();
+            forePen = new Pen(ForeColor);
+        }
+
+        private bool on;
+        public bool On
+        {
+            get
+            {
+                return on;
+            }
+            set
+            {
+                on = value;
+                Invalidate();
+            }
+        }
+
+        private void StatusLight_Paint(object sender, PaintEventArgs e)
+        {
+            forePen.Color = ForeColor;
+            
+            Graphics g = e.Graphics;
+            g.DrawEllipse(forePen, 0, 0, Size.Width, Size.Height);
+        }
+    }
+}
